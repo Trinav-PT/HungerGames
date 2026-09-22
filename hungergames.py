a013a4232316a075154331b3f6233cc04b656d0b
Trinav Prasad Talukdar <144201740+Trinav-PT@users.noreply.github.com>
2026-09-22 19:27:24 +0530

    Update hungergames.py

--- hungergames.py
+++ hungergames.py
@@ -786,7 +786,7 @@ def show_credits():
 <div style="font-family:'Cinzel',serif;font-size:1.45rem;color:#f1f1f1;">Chewie<br>your own name =)</div>
 </div>
 <div style="font-family:'Cinzel',serif;font-size:1.15rem;color:#c52c2c;margin-top:3.5rem;letter-spacing:1px;line-height:1.6;">
-...you really scrolled till the end?<br>EXCELLENT. TRULY EXCELLENT.
+...you really scrolled till the end?<br>EXCELLENT. TRULY EXCELLENT. not like you had a choice...
 </div>
 </div>
 """, unsafe_allow_html=True)
